c921558c74eabecec39f1f2b72b6b9307591cea2
codebyella <codebyella@gmail.com>
2024-07-30 00:13:23 -0300

    ver

--- app/src/main/java/br/com/academytechmaster/pookotlinencapsulamento/Livro.kt
+++ app/src/main/java/br/com/academytechmaster/pookotlinencapsulamento/Livro.kt
@@ -2,10 +2,48 @@ package br.com.academytechmaster.pookotlinencapsulamento
 
 class Livro(
 
-    var titulo: String,
-    var autor: String,
-    var editora: String,
-    var localizacao: String,
-    var paginas: Int
+    private var _titulo: String,
+    private var _autor: String,
+    private var _editora: String,
+    private var _localizacao: String,
+    private var _paginas: Int
 
-)
+){
+    var titulo: String
+        get() = _titulo
+        set(valor) {
+            _titulo = valor
+        }
+
+    var autor: String
+        get() = _autor
+        set(valor) {
+            _autor = valor
+        }
+
+    var editora: String
+        get() = _editora
+        set(valor) {
+            _editora = valor
+        }
+
+    var localizacao: String
+        get() = _localizacao
+        set(valor) {
+            _localizacao = valor
+        }
+
+    var paginas: Int
+        get() = _paginas
+        set(valor) {
+            _paginas = valor
+        }
+
+    fun detalhes(){
+        println("Título: $titulo")
+        println("Autor: $autor")
+        println("Editora: $editora")
+        println("Localização: $localizacao")
+        println("Páginas: $paginas")
+    }
+}
